818af742f7d07fd1b4ba6f1257c49e9fe0611ee2
Hannes Loeffler <Hannes.Loeffler@gmail.com>
2023-11-10 19:48:33 +0100

    simple approach: return severity code which is expected to be reverse transformed

--- contrib/reinvent_plugins/components/NIBRSubstructureFilters/comp_nibr_substructure_filter.py
+++ contrib/reinvent_plugins/components/NIBRSubstructureFilters/comp_nibr_substructure_filter.py
@@ -7,6 +7,7 @@ and adapted as needed.  The original CSV file was converted to pickle file.
 
 __all__ = ["NIBRSubstructureFilters"]
 #from dataclasses import dataclass
+import os
 import pickle
 from typing import List
 import logging
@@ -15,9 +16,9 @@ import numpy as np
 from rdkit import Chem
 
 from .assign_filters import assign_filters
-from .component_results import ComponentResults
+from ..component_results import ComponentResults
 from reinvent_plugins.mol_cache import molcache
-from .add_tag import add_tag
+from ..add_tag import add_tag
 
 logger = logging.getLogger('reinvent')
 
@@ -30,14 +31,23 @@ logger = logging.getLogger('reinvent')
 
 @add_tag("__component", "filter")
 class NIBRSubstructureFilters:
-    def __init__(self, params: Parameters):
-        with open("catalog.pkl", "rb") as pfile:
+    def __init__(self, *args, **kwargs):  #params: Parameters):
+        path = os.path.dirname(__file__)
+        catalog_filename = os.path.join(path, "catalog.pkl")
+
+        with open(catalog_filename, "rb") as pfile:
             self.catalog = pickle.load(pfile)
 
     @molcache
     def __call__(self, mols: List[Chem.Mol]) -> np.array:
         scores = []
 
+        # SubstructureMatches, Min_N_O_filter, Frac_N_O, Covalent,
+        # SpecialMol, SeverityScore
+
         nibr_scores = assign_filters(self.catalog, mols)
+        
+        for entry in nibr_scores:
+            scores.append((entry.SeverityScore))
 
         return ComponentResults([np.array(scores, dtype=float)])
