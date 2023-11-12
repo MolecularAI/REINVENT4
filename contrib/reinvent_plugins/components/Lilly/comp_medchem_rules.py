8af9d9c683120a6bc06e076e5be8f4c81464a8ce
Hannes Loeffler <Hannes.Loeffler@gmail.com>
2023-11-12 10:37:23 +0100

    added Lilly Medchem rules as scoring component

---
+++ contrib/reinvent_plugins/components/Lilly/comp_medchem_rules.py
@@ -0,0 +1,118 @@
+"""Iain Watson's Lilly Medchem rules
+
+Compute the demerit score for each SMILES.  Not implemented as a filter.
+"""
+
+__all__ = ["LillyMedchemRules"]
+import re
+import tempfile
+from dataclasses import dataclass
+from typing import List
+import logging
+
+import numpy as np
+from rdkit import Chem
+
+from ..run_program import run_command
+from ..component_results import ComponentResults
+from ..add_tag import add_tag
+
+logger = logging.getLogger("reinvent")
+
+CMD = "Lilly_Medchem_Rules.rb"  # assume in path
+
+
+@add_tag("__parameters")
+@dataclass
+class Parameters:
+    relaxed: List[bool]  # 7-50 heavy atoms, 160 demerit cutoff
+
+
+@add_tag("__component")
+class LillyMedchemRules:
+    def __init__(self, params: Parameters):
+        self.want_relaxed = params.relaxed
+
+    def __call__(self, smilies: List[str]) -> np.array:
+        scores = []
+
+        for want_relaxed in self.want_relaxed:
+            cmd = [CMD]
+
+            if want_relaxed:
+                cmd.append("-relaxed")
+
+            with (tempfile.NamedTemporaryFile(mode="w+", suffix=".smi", delete=True) as in_smi,):
+                for num, smiles in enumerate(smilies):
+                    in_smi.write(f"{smiles} ID{num}\n")
+
+                in_smi.flush()
+
+                cmd.append(in_smi.name)
+                result = run_command(cmd)
+
+            demerits = parse_output(result.stdout, smilies)
+            scores.append(np.array(demerits, dtype=float))
+
+        return ComponentResults(scores)
+
+
+# \1 SMILES
+# \2 ID number
+# \3 reason for rejection
+BAD_PATTERN = re.compile(r"(.*?) ID(\d+) (.*)")
+
+# \1 SMILES
+# \2 ID number
+# \3 (demerit string, optional)
+# \4 demerit value, optional
+# \5 reason for demerits, optional
+DEMERIT_PATTERN = re.compile(r"(.*?) ID(\d+)( : D\((\d+)\) (.*))?")
+
+
+def parse_output(lines: str, smilies: List[str]) -> List[float]:
+    """Parse the output from the medchem rule tool
+
+    Note: RDKit canonicalization of the Lilly SMILES may not string match
+          with the original canonical SMILES because Lilly has its own
+          chemistry model!  Hence match by ID.
+    """
+
+    # bad0.smi: mc_first_pass
+    mc_smilies = {}
+
+    with open("bad0.smi", "r") as mc_bad:
+        for line in mc_bad:
+            match = BAD_PATTERN.match(line)
+            ID = match.group(2)
+            mc_smilies[ID] = np.NaN
+
+    # bad1.smi, bad2.smi: tsubstructure - rejections 1 and 2
+    for filename in ("bad1.smi", "bad2.smi"):
+        with open(filename, "r") as tsub_bad:
+            for line in tsub_bad:
+                match = BAD_PATTERN.match(line)
+                ID = match.group(2)
+                mc_smilies[ID] = np.NaN
+
+    # bad3.smi: iwdemerit
+    with open("bad3.smi", "r") as mc_bad:
+        for line in mc_bad:
+            match = DEMERIT_PATTERN.match(line)
+            ID = match.group(2)
+            demerit = match.group(4)
+            mc_smilies[ID] = int(demerit)
+
+    for line in lines.splitlines():
+        match = DEMERIT_PATTERN.match(line)
+        ID = match.group(2)
+        demerit = match.group(4)
+
+        if not match.group(3):  # not demerits
+            mc_smilies[ID] = 0
+        else:
+            mc_smilies[ID] = int(demerit)
+
+    scores = list(dict(sorted(mc_smilies.items())).values())
+
+    return scores
---
+++ contrib/tests/reinvent_plugins/unit_tests/components/test_comp_lilly_medchem_rules.py
@@ -0,0 +1,34 @@
+import pytest
+
+import numpy as np
+
+from reinvent_plugins.components.Lilly.comp_medchem_rules import Parameters
+from reinvent_plugins.components.Lilly.comp_medchem_rules import LillyMedchemRules
+
+SMILIES = [
+    "Cc1ccc(-n2[nH]c(C)c(N=Nc3cccc(-c4cccc(C(=O)O)c4)c3O)c2=O)cc1C",
+    "CC(=O)Oc1ccccc1C(=O)O",
+    "Cc1ccc(-c2cc(C(F)(F)F)nn2-c2ccc(S(N)(=O)=O)cc2)cc1",
+    "CC(C)Cc1ccc([C@@H](C)C(=O)O)cc1",
+    "Cc1c(N(C)CS(=O)(=O)O)c(=O)n(-c2ccccc2)n1C",
+    "Cn1c(=O)c2c(ncn2C)n(C)c1=O",
+    "COC(=O)C1C(OC(=O)c2ccccc2)CC2CCC1N2C",
+    "Clc1ccc2c(c1)C(N1CCNCC1)=Nc1ccccc1O2",
+    "CNC(C)C1CCC(N)C(OC2C(N)CC(N)C(OC3OCC(C)(O)C(NC)C3O)C2O)O1",
+]
+
+
+@pytest.mark.parametrize(
+    "relaxed, expected_results",
+    [
+        ([False], [153, np.nan, 6, 0, 40, 0, 70, 50, 253]),
+        ([True],  [189, np.nan, 0, 0, 40, 0, 70, 50, 229]),
+    ],
+)
+def test_comp_lilly_medchem_rules(relaxed, expected_results):
+    params = Parameters(relaxed)
+    rules = LillyMedchemRules(params)
+    results = rules(SMILIES)
+    expected = np.array(expected_results)
+
+    assert ((results.scores == expected) | (np.isnan(results.scores) & np.isnan(expected))).all()
