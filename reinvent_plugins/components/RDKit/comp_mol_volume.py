729adccc3eb0d845bb0f9d121699209af84f3bc6
Löffler, Hannes <hannes.loffler@astrazeneca.com>
2023-12-05 10:57:27 +0100

    added MolVolume scoring component updated tests

---
+++ reinvent_plugins/components/RDKit/comp_mol_volume.py
@@ -0,0 +1,53 @@
+"""Compute the RDKit molecule volume from a conformer and its grid-encoding
+
+The quality depends on the quality of the conformer.
+"""
+
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+import numpy as np
+from rdkit.Chem import AllChem as Chem
+
+from ..component_results import ComponentResults
+from ..add_tag import add_tag
+from reinvent_plugins.mol_cache import molcache
+
+
+@add_tag("__parameters")
+@dataclass
+class Parameters:
+    grid_spacing: Optional[List[float]] = field(default_factory=lambda: [0.2])
+    box_margin: Optional[List[float]] = field(default_factory=lambda: [2.0])
+
+
+@add_tag("__component")
+class MolVolume:
+    """Compute the molecular volume from the grid of the molecule's shape"""
+
+    def __init__(self, params: Parameters):
+        self.grid_spacings = params.grid_spacing
+        self.box_margin = params.box_margin
+
+    @molcache
+    def __call__(self, mols: List[Chem.Mol]) -> np.array:
+        scores = []
+
+        for grid_spacing, box_margin in zip(self.grid_spacings, self.box_margin):
+            volumes = []
+
+            for mol in mols:
+                try:
+                    mol3d = Chem.AddHs(mol)  # will not consider protonation state
+                    Chem.EmbedMolecule(mol3d)
+                    volume = Chem.ComputeMolVolume(
+                        mol3d, gridSpacing=grid_spacing, boxMargin=box_margin
+                    )
+                except ValueError:
+                    volume = np.nan
+
+                volumes.append(volume)
+
+            scores.append(np.array(volumes, dtype=float))
+
+        return ComponentResults(scores)
--- tests/reinvent_plugins/unit_tests/components/RDKit/test_comp_pmi.py
+++ tests/reinvent_plugins/unit_tests/components/RDKit/test_comp_pmi.py
@@ -1,12 +1,50 @@
+"""Test for the RDKit PMI descriptor
+
+NOTE: This is a 3D descriptor and therefore the scoring component creates
+      a conformer.  However, this is not deterministic meaning that the
+      generated 3D coordinates can vary depending on the flexibility of the
+      molecule and the quality of the conformer generator.  The examples
+      here are basically rigid (except for a rotating methyl group).
+
+      This also means that the test cases here are not really unit tests.
+"""
+
 import numpy as np
 
 from reinvent_plugins.components.RDKit.comp_pmi import Parameters, PMI
 
 
-def test_comp_pmi():
-    params = Parameters(["npr1", "npr2"])
+RTOL = 0.05  # this makes the tests fairly fragile
+
+
+def test_comp_pmi_all():
     smiles = ["c1ccccc1", "Cc1ccccc1"]
+    expected_results = [np.array([0.487, 0.312]), np.array([0.513, 0.700])]
+
+    params = Parameters(["npr1", "npr2"])
     pmi = PMI(params)
     results = pmi(smiles)
-    expected_results = np.array([0.48749534, 0.31194547, 0.51250466, 0.69953459])
-    assert np.allclose(np.concatenate(results.scores), expected_results)
+
+    assert np.allclose(results.scores, expected_results, rtol=RTOL)
+
+
+def test_comp_pmi_npr1():
+    smiles = ["c1ccccc1", "Cc1ccccc1"]
+    expected_results = np.array([0.487, 0.312])
+
+    params = Parameters(["npr1"])
+    pmi = PMI(params)
+    results = pmi(smiles)
+
+    assert np.allclose(results.scores, expected_results, rtol=RTOL)
+
+
+def test_comp_pmi_npr2():
+    smiles = ["c1ccccc1", "Cc1ccccc1"]
+    expected_results = np.array([0.513, 0.700])
+
+    params = Parameters(["npr2"])
+    pmi = PMI(params)
+    results = pmi(smiles)
+
+    assert np.allclose(results.scores, expected_results, rtol=RTOL)
