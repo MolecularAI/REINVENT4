ee083e17032ea966e4e01d532d6cd8e3bc8fa21e
hampusgs <hampusgs@gmail.com>
2025-10-29 10:10:33 +0100

    Change back to original behaviour for setting sample_strategy value to isolate new changes

--- reinvent/runmodes/setup_sampler.py
+++ reinvent/runmodes/setup_sampler.py
@@ -49,8 +49,10 @@ def setup_sampler(model_type: str, config: dict, agent: ModelAdapter):
             FutureWarning,
             stacklevel=2,
         )
-
-    sample_strategy = config.get("sample_strategy", "multinomial")
+    if model_type in TRANSFORMERS:
+        sample_strategy = config.get("sample_strategy", "multinomial")
+    else:
+        sample_strategy = config.get("sample_strategy", None)
 
     isomeric = False
 
