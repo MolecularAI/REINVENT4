7d4e72a71bdad56b7fb7afae7078a0aa348cfec2
aalexpersson <alex.persson0408@gmail.com>
2026-01-29 16:06:42 +0100

    display tensorboard dynamic dir

--- notebooks/display_tb.py
+++ notebooks/display_tb.py
@@ -3,12 +3,19 @@ from tensorboard import program
 import os
 # %load_ext tensorboard
 
-wd = f"{os.getcwd()}/tmp"
+wd = f"{os.getcwd()}/runs"
+
+# Set working directory to the latest subdirectory in runs
+subdirs = [d for d in os.listdir(wd) if os.path.isdir(os.path.join(wd, d))]
+latest_subdir = max(subdirs, key=lambda d: os.path.getmtime(os.path.join(wd, d)))
+wd = os.path.join(wd, latest_subdir)
+
+print("Current working directory to visualise in tensorboard:", wd)
+
 logdir = f"{wd}/tb_stage1_0"
 tb = program.TensorBoard()
 tb.configure(argv=[None, "--bind_all", "--logdir", logdir, "--port", "8089"])
 url = tb.launch()
-print(wd)
 
 print(f"TensorBoard is running at {url}")
 input("Press Enter to terminate...")
