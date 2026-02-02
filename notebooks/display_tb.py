575a82df15e8080e58dfe5bcb248d5ea5926f36d
aalexpersson <alex.persson0408@gmail.com>
2026-02-02 10:26:36 +0100

    display tensorboard updated for new dir-tree

--- notebooks/display_tb.py
+++ notebooks/display_tb.py
@@ -1,18 +1,20 @@
+#FIXME: Running requires explicit port forwarding in vscode atleast for eduroam.
+
 from tensorboard import program
 import os
 
+# Define the working directory
 wd = f"{os.getcwd()}/runs"
 
 # Set working directory to the latest subdirectory in runs
 subdirs = [d for d in os.listdir(wd) if os.path.isdir(os.path.join(wd, d))]
 latest_subdir = max(subdirs, key=lambda d: os.path.getmtime(os.path.join(wd, d)))
 wd = os.path.join(wd, latest_subdir)
+print("Launching TensorBoard with logdir:", wd)
 
-print("Current working directory to visualise in tensorboard:", wd)
-
-logdir = f"{wd}/tb"
+# Launch TensorBoard
 tb = program.TensorBoard()
-tb.configure(argv=[None, "--bind_all", "--logdir", logdir, "--port", "8089"])
+tb.configure(argv=[None, "--bind_all", "--logdir", wd, "--port", "8089"])
 url = tb.launch()
 
 print(f"TensorBoard is running at {url}")
